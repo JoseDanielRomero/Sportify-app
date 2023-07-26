6f0b851a47f47973b12b4bc8d1d6887f95c76e0d
Jose Daniel Romero <danyrome12@gmail.com>
2023-07-25 20:03:07 -0500

    Add header in dashboard page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import { HashRouter, Route, Router } from 'react-router-dom';
+import { createContext, useState } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import DashboardPage from './routes/DashboardPage';
 
+export const ContentContext = createContext([])
+
 function App() {
+  const options = [
+    {value: 'league', text: 'LEAGUES'},
+    {value: 'team', text: 'TEAMS'},
+  ]
+
+  const [actualContent, setActualContent] = useState(options[0].value)
+
   return (
-    <HashRouter>
-      <Router>
-        <Route path='/' element={<DashboardPage />} />
-      </Router>
-    </HashRouter>
+    <ContentContext.Provider value={{ actualContent, setActualContent }}>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<DashboardPage options={options} />} />
+        </Routes>
+      </HashRouter>
+    </ContentContext.Provider>
   );
 }
 
--- src/routes/DashboardPage.js
+++ src/routes/DashboardPage.js
@@ -1,8 +1,39 @@
 import '../stylesheets/DashboardPage.css'
+import logoIcon from '../images/logo.svg'
+import { useContext } from 'react'
+import { ContentContext } from '../App'
+import triangleLeft from '../images/triangle-left.svg'
+import triangleRight from '../images/triangle-right.svg'
 
-function DashboardPage() {
+function DashboardPage({ options }) {
+
+  const { actualContent, setActualContent } = useContext(ContentContext)
+
+  const handleChangeSelectbox = (event) => {
+    setActualContent(event.target.value)
+  }
+
   return (
-    <div>DashboardPage</div>
+    <div className='DashboardPage'>
+      <header className='header'>
+        <div className='logo-box'>
+          <img className='logo-image' src={logoIcon}/>
+          <h1 className='logo-text'>SPORTIFY</h1>
+        </div>
+        <div className='dropdown-box'>
+          <img className='triangle-for-button' src={triangleLeft} />
+          <select value={actualContent} className='selectbox-form' onChange={handleChangeSelectbox} >
+            {options.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.text}
+              </option>
+            ))}
+          </select>
+          <img className='triangle-for-button' src={triangleRight} />
+        </div>
+
+      </header>
+    </div>
   )
 }
 
